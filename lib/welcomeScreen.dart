7ac6ae3cb299a81107f820244460d2735eef3607
exam <exam@Aptechnk1.edu>
2024-09-26 20:54:33 +0500

    Abdullah

--- lib/welcomeScreen.dart
+++ lib/welcomeScreen.dart
@@ -7,13 +7,22 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          color: Colors.black,
-          height: 200,
-          width: 200,
-          child: Text(
-            "Rohaan",
-            style: TextStyle(color: Colors.white),
-          )),
+        //margin: EdgeInsets.only(left:20),
+        //margin:EdgeInsets.fromLTRB(20, 10, 10, 10),
+        //padding: EdgeInsets.symmetric(horizontal: 50,vertical: 100),
+        //padding: EdgeInsets.all(20),
+        //padding: EdgeInsets.only(left: 100,bottom: 50,right: 10),
+        color: Colors.black,
+        height: 200,
+        width: 200,
+        child: const Center(
+            child: Text(
+          textAlign: TextAlign.center,
+          "Abdullah",
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontSize: 25, color: Colors.white),
+        )),
+      ),
     );
   }
 }
